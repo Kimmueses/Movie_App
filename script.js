09d37c92f76dd26b3c6f057cbbc0cd46fbb86922
Kimberly MB <k.mueses@yahoo.com>
2023-02-26 22:58:04 -0500

    Creates JS variables with endpoints & function

---
+++ script.js
@@ -0,0 +1,14 @@
+const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=663c8b50d3af766415806c8a9c66b2e2&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_URL = 'http://api.themoviedb.org/3/search/movie?api_key=663c8b50d3af766415806c8a9c66b2e2&query="'
+
+// Get initial movies 
+getMovies(API_URL)
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  console.log(data.results)
+}
+

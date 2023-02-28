2e8d55d20cb3ef079cfbd99e968dcb1345f6af9e
Kimberly MB <k.mueses@yahoo.com>
2023-02-27 20:37:41 -0500

    Adds form event listener for search functionality

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=663c8b50d3af766415806c8a9c66b2e2&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_URL = 'http://api.themoviedb.org/3/search/movie?api_key=663c8b50d3af766415806c8a9c66b2e2&query="'
+const SEARCH_API = 'http://api.themoviedb.org/3/search/movie?api_key=663c8b50d3af766415806c8a9c66b2e2&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
 // Get initial movies 
 getMovies(API_URL)
@@ -12,3 +15,16 @@ async function getMovies(url) {
   console.log(data.results)
 }
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search.value
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm)
+
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})

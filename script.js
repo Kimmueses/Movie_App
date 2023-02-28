306fba035c05b20bb9bb6261bb624caafc4f877c
Kimberly MB <k.mueses@yahoo.com>
2023-02-27 21:12:42 -0500

    Inserts fetched data into DOM

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'http://api.themoviedb.org/3/search/movie?api_key=663c8b50d3af766415806c8a9c66b2e2&query="'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -12,9 +13,46 @@ async function getMovies(url) {
   const res = await fetch(url)
   const data = await res.json()
 
-  console.log(data.results)
+  showMovies(data.results)
 }
 
+function showMovies(movies) {
+  main.innerHTML = ''
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+
+    movieEl.innerHTML = `    
+      <img src="${IMG_PATH + poster_path}" alt="${title}">
+      <div class="movie-info">
+    <h3>${title}</h3>
+    <span class="${getClassByRate}">${vote_average}</span>
+      </div>
+      <div class="overview">
+    <h3>Overview</h3>
+    ${overview}
+    </div>
+  </div>`
+
+  main.appendChild(movieEl)
+  })
+}
+
+// Function for voting average 
+function getClassByRate(vote) {
+  if(vote >= 8) {
+    return 'green'
+  } else if(vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
+}
+
+// Form event listener and search arrow function
 form.addEventListener('submit', (e) => {
   e.preventDefault()
 

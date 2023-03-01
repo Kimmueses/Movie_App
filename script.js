cd86fdd072260052418b205201d546d186814abc
Kimberly MB <k.mueses@yahoo.com>
2023-02-28 21:15:18 -0500

    Adds  parameter to span class

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function showMovies(movies) {
 
   movies.forEach((movie) => {
     const { title, poster_path, vote_average, overview } = movie
-
+    
     const movieEl = document.createElement('div')
     movieEl.classList.add('movie')
 
@@ -29,7 +29,7 @@ function showMovies(movies) {
       <img src="${IMG_PATH + poster_path}" alt="${title}">
       <div class="movie-info">
     <h3>${title}</h3>
-    <span class="${getClassByRate}">${vote_average}</span>
+    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
       </div>
       <div class="overview">
     <h3>Overview</h3>
